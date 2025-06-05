5b28d334d52db0c5b24a90a2be4a0cf65bda6979
Prashant Kumar <kumarprashant71037@gmail.com>
2025-06-05 17:31:29 +0530

    database connection updated

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
+  private baseUrl = environment.apiUrl;
+  
   constructor(private _http: HttpClient) {}
 
   addEmployee(data: any): Observable<any> {
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://health-tracker-backend-lutk.onrender.com'
+};
